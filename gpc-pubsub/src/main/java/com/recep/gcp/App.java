9cb6ee8630a9726ae4dee1da809887cfdab269f5
Ayaz <rpaz@gft.com>
2019-10-18 07:00:08 +0100

    First Pubsub Project

---
+++ gpc-pubsub/src/main/java/com/recep/gcp/App.java
@@ -0,0 +1,13 @@
+package com.recep.gcp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
